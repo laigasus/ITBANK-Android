3bd589e24f4611f230ec72a42e8138e23be38242
laigasus <okjaeook98@gmail.com>
2020-07-25 11:01:50 +0900

    .

---
+++ 강의/안드로이드 2일차 실습/4. java로 레이아웃 다루기/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextView textView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout mainLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(mainLayout, params);
+
+        editText = new EditText(this);
+        editText.setHint("여기에 입력하세요");
+
+        mainLayout.addView(editText);
+
+        button = new Button(this);
+        button.setText("버튼입니다");
+        button.setBackgroundColor(Color.YELLOW);
+        mainLayout.addView(button);
+
+        textView = new TextView(this);
+        textView.setText("텍스트뷰입니다.");
+        textView.setTextSize(20);
+        textView.setTextColor(Color.RED);
+        mainLayout.addView(textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+
+
+    }
+
+}
---
+++ 강의/안드로이드 2일차 실습/8. 이벤트 처리/각각 리스너 생성MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+
+    Button[] numButtons = new Button[10];
+    Integer[] numBtnIDs = { R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2,
+            R.id.BtnNum3, R.id.BtnNum4, R.id.BtnNum5, R.id.BtnNum6,
+            R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9 };
+    int i;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        btnSub = (Button) findViewById(R.id.BtnSub);
+        btnMul = (Button) findViewById(R.id.BtnMul);
+        btnDiv = (Button) findViewById(R.id.BtnDiv);
+
+        textResult = (TextView) findViewById(R.id.TextResult);
+
+        for (i = 0; i < numBtnIDs.length; i++) {
+            numButtons[i] = (Button) findViewById(numBtnIDs[i]);
+        }
+
+        for (i = 0; i < numBtnIDs.length; i++) {
+            final int index;
+            index = i;
+            numButtons[index].setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    num1 = edit1.getText().toString()
+                            + numButtons[index].getText().toString();
+                    edit1.setText(num1);
+                }
+            });
+        }
+    }
+
+}
