bda95d7fcb0fcca552b027ddcc457ab221e06b82
laigasus <okjaeook98@gmail.com>
2020-07-26 22:51:00 +0900

    4. 뷰플리퍼

---
+++ 20200725/04ScrollView/app/src/main/java/com/chapter03/a04scrollview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.chapter03.a04scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+    Button totop, tobottom;
+    ScrollView scrollView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        totop = (Button) findViewById(R.id.totop);
+        tobottom = (Button) findViewById(R.id.tobottom);
+        scrollView = (ScrollView) findViewById(R.id.scrollView1);
+
+        totop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrollView.fullScroll(ScrollView.FOCUS_UP);
+            }
+        });
+
+        tobottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+
+
+    }
+}
