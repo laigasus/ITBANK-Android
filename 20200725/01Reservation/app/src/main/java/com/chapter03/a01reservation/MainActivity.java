565f99867e3638f95ca6a5dc3c4323d2f1653696
laigasus <okjaeook98@gmail.com>
2020-07-26 22:45:50 +0900

    1. 예약 어플

---
+++ 20200725/01Reservation/app/src/main/java/com/chapter03/a01reservation/MainActivity.java
@@ -0,0 +1,90 @@
+package com.chapter03.a01reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    Chronometer chrono;
+    RadioButton rdoDate, rdoTime;
+    TimePicker timePicker;
+    DatePicker datePicker;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+        rdoDate = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+        timePicker = (TimePicker) findViewById(R.id.timePicker1);
+        datePicker = (DatePicker) findViewById(R.id.datePicker1);
+        textView = (TextView) findViewById(R.id.textview1);
+
+        rdoDate.setVisibility(View.INVISIBLE);
+        rdoTime.setVisibility(View.INVISIBLE);
+        timePicker.setVisibility(View.INVISIBLE);
+        datePicker.setVisibility(View.INVISIBLE);
+
+        chrono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+                rdoDate.setVisibility(View.VISIBLE);
+                rdoTime.setVisibility(View.VISIBLE);
+            }
+        });
+
+        chrono.setOnLongClickListener(new View.OnLongClickListener() {
+            public boolean onLongClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+
+                textView.setText(
+                        Integer.toString(datePicker.getYear()) + "년 "
+                                + Integer.toString(1 + datePicker.getMonth()) + "월 "
+                                + Integer.toString(datePicker.getDayOfMonth()) + "일 "
+                                + Integer.toString(timePicker.getCurrentHour()) + "시 "
+                                + Integer.toString(timePicker.getCurrentMinute()) + "분 예약됨"
+                );
+
+                rdoDate.setVisibility(View.INVISIBLE);
+                rdoTime.setVisibility(View.INVISIBLE);
+                timePicker.setVisibility(View.INVISIBLE);
+                datePicker.setVisibility(View.INVISIBLE);
+                return false;
+
+            }
+        });
+        rdoDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePicker.setVisibility(View.INVISIBLE);
+                datePicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePicker.setVisibility(View.VISIBLE);
+                datePicker.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
