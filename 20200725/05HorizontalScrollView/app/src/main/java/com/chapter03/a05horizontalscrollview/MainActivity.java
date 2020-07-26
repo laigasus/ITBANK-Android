aa5c8a951da22cea7964c27532b41b31dc0cdb1d
laigasus <okjaeook98@gmail.com>
2020-07-26 22:52:32 +0900

    5. 수직 스크롤뷰

---
+++ 20200725/05HorizontalScrollView/app/src/main/java/com/chapter03/a05horizontalscrollview/MainActivity.java
@@ -0,0 +1,40 @@
+package com.chapter03.a05horizontalscrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+    Button toleft, toright;
+    HorizontalScrollView horizontalscrollView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toleft = (Button) findViewById(R.id.toleft);
+        toright = (Button) findViewById(R.id.toright);
+        horizontalscrollView = (HorizontalScrollView) findViewById(R.id.horizontalscrollView1);
+
+        toleft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                horizontalscrollView.fullScroll(ScrollView.FOCUS_LEFT);
+            }
+        });
+
+        toright.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                horizontalscrollView.fullScroll(ScrollView.FOCUS_RIGHT);
+            }
+        });
+
+
+    }
+}
